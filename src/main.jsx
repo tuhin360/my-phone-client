662dc1d47ce2f7569b263b626301dd1d1a229e91
tuhin <tuhin153695@gmail.com>
2023-04-17 13:45:21 +0600

    phone client server

---
+++ src/main.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Main from './components/Main';
+import Phones from './components/Phones';
+import Phone from './components/Phone';
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main />,
+    children: [
+      {
+        path: "/phones",
+        element: <Phones />,
+        loader: () => fetch('http://localhost:5000/phones')
+      },
+      {
+        path: '/phone/:id',
+        element : <Phone></Phone>,
+        loader: ({params}) => fetch(`http://localhost:5000/phones/${params.id}`)
+      }
+    ],
+  },
+]);
+
+
+
+
+
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+     <RouterProvider router={router}></RouterProvider>
+  </React.StrictMode>,
+)
